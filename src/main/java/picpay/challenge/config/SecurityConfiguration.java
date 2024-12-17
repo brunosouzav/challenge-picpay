792f2ae37b01c13aa2b41e6f8583440761f815b3
brunosouzav <brunocloud78@gmail.com>
2024-12-17 16:37:00 -0300

    feat: Implementação da configuração de segurança e aplicação de filtro JWT
    - Adicionada a configuração de segurança para desabilitar CSRF e tornar a aplicação Stateless.
    - Implementado o filtro  para validar e autenticar tokens JWT nas requisições.
    - Configuração de gerenciamento de sessão com política , adequada para autenticação baseada em JWT.
    - Definido acesso público para endpoints de autenticação () e exigido autenticação para todas as outras requisições.
    - Adicionada a configuração de filtro JWT antes do filtro padrão de autenticação .

---
+++ src/main/java/picpay/challenge/config/SecurityConfiguration.java
@@ -0,0 +1,42 @@
+package picpay.challenge.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+
+    private JwtAuthenticationFilter jwtAuthFilter;
+
+
+    private AuthenticationProvider authenticationProvider;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtAuthenticationFilter jwtAuthenticationFilter) throws Exception {
+        return  http.csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+
+
+                .build();
+
+    }
+}
